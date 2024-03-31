118ea8f864bad7807559dcb4c02e20d94797bd94
saluttolove <124430597+saluttolove@users.noreply.github.com>
2024-03-31 11:58:02 +0200

    #54 add some change in header and designed template for title and button

--- src/components/views/Header.tsx
+++ src/components/views/Header.tsx
@@ -13,7 +13,7 @@ import "../../styles/views/Header.scss";
 
 const Header = props => (
   <div className="header container" style={{height: props.height}}>
-    <h1 className="header title">SoPra FS24 Group 18 The Price Is Right</h1>
+    <h1 className="header title">SoPra FS24 Group 18 </h1>
   </div>
 );
 
