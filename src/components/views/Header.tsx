ac7c65d3f9e33c872dc90f4845aff2ad0ae86714
Xuexue1111 <xuexxx11@gmail.com>
2024-03-31 23:09:48 +0200

    close #23 registraion logic

--- src/components/views/Header.tsx
+++ src/components/views/Header.tsx
@@ -7,13 +7,14 @@ import "../../styles/views/Header.scss";
  * Conceptually, components are like JavaScript functions. They accept arbitrary inputs (called “props”) and return React elements describing what should appear on the screen.
  * They are reusable pieces, and think about each piece in isolation.
  * Functional components have to return always something. However, they don't need a "render()" method.
- * https://react.dev/learn/your-first-component and https://react.dev/learn/passing-props-to-a-component 
+ * https://react.dev/learn/your-first-component and https://react.dev/learn/passing-props-to-a-component
  * @FunctionalComponent
  */
 
 const Header = props => (
-  <div className="header container" style={{height: props.height}}>
-    <h1 className="header title">SoPra FS24 Group 18 </h1>
+//   <div className="header container" style={{height: props.height}}>
+  <div className="header container" style={{height: "auto"}}>
+     <h1 className="header title">SoPra FS24 Group 18 </h1>
   </div>
 );
 
