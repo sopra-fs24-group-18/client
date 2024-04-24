074c4da8034425c60d1ceba2fd8284cb4171d789
Lu Li <stull2023@163.com>
2024-04-24 19:57:40 +0200

    intergrate Gameroom.tsx

--- src/components/views/Shop.tsx
+++ src/components/views/Shop.tsx
@@ -10,138 +10,148 @@ import PropTypes from "prop-types";
 
 
 const Purchase = () => {
-  const navigate = useNavigate();
-  const [timeLeft, setTimeLeft] = useState(30);
-  const [message, setMessage] = useState({ text: "", type: "" });
+    const navigate = useNavigate();
+    const [timeLeft, setTimeLeft] = useState(30);
+    const [message, setMessage] = useState({ text: "", type: "" });
 
-  const [user, setUser] = useState<User[]>([]);
-  const [tools, setTools] = useState<Tool[]>([]);
+    const [player, setPlayer] = useState("");
 
-  const userId = localStorage.getItem("current_user_id");
-  // localStorage.setItem("score", 100);
-  // fetch current user data
-  useEffect(() => {
-    async function fetchUser() {
-      try {
-        const response = await api.get(`/users/${userId}`);
-        setUser(response.data);
-        console.log("User data fetched successfully:", response.data);
-      } catch (error) {
-        console.error(`Something went wrong while fetching the user: \n${handleError(error)}`);
-      }
-    }
+    const userId = localStorage.getItem("userId");
+    // fetch current user data
+    useEffect(() => {
+        async function fetchUser() {
+            try {
+                const response = await api.get(`/users/${userId}`);
+                setPlayer(response.data);
+                console.log("User data fetched successfully:", response.data);
+            } catch (error) {
+                console.error(`Something went wrong while fetching the user: \n${handleError(error)}`);
+            }
+        }
+        fetchUser();
+    }, [userId]);
 
-    fetchUser();
-  }, [userId]);
+    // timer
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTimeLeft(timeLeft - 1);
+        }, 1000);
+        if (timeLeft === 0) {
+            clearTimeout(timer);
+            navigate("/shop");
+        }
+        return () => clearTimeout(timer);
+    }, [timeLeft]);
 
-  // timer
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(timeLeft - 1);
-    }, 1000);
-    if (timeLeft === 0) {
-      clearTimeout(timer);
-      navigate("/users/:userId/shop");
-    }
-    return () => clearTimeout(timer);
-  }, [timeLeft]);
+    const doPurchase = async (toolType) => {
+        try {
+            const response = await api.get(`/tools`);
+            if (toolType === response.data[0].type){
+                const tool = new Tool(response.data[0]);
+                const toolId = tool.id;
+                const requestBody = JSON.stringify({ toolId, userId });
+                const response_2 = await api.post(`/tools/${toolId}/${userId}`, requestBody);
+            }else if (toolType === response.data[1].type){
+                const tool = new Tool(response.data[1]);
+                const toolId = tool.id;
+                const requestBody = JSON.stringify({ toolId, userId });
+                const response_2 = await api.post(`/tools/${toolId}/${userId}`, requestBody);
+            }else{
+                displayMessage("Cannot find this tool.", "error-message");
+            }
+            //const requestBody = JSON.stringify({ toolId, userId });
+            //const response = await api.post(`/tools/${toolId}/${userId}`, requestBody);
 
-  const doPurchase = async (toolType) => {
-    try {
+            // Show success message
+            //displayMessage(`Buy and use the ${toolType} successfully!`, "success-message");
 
-  // purchase successfully worked --> navigate to the gameRoom
-  //    navigate("/gameRoom");
+        } catch (error) {
+            if (error.response && error.response.status === 403) {
+                displayMessage("You don't have enough points.", "error-message");
+            }
+            else{
+                displayMessage(`Something went wrong during the purchase: ${handleError(error)}`, "error-message");
+            }
+        }
+    };
 
-      // Show success message
-      // displayMessage(`Buy and use the ${toolType} successfully!`, "success-message");
+    const displayMessage = (messageText, messageType) => {
+        setMessage({ text: messageText, type: messageType });
+        setTimeout(() => {
+            setMessage({ text: "", type: "" });
+        }, 5000); // Hide message after 5 seconds
+    };
 
-    } catch (error) {
-      if (error.response && error.response.status === 403) {
-        displayMessage("You don't have enough points.", "error-message");
-      }
-      else{
-        displayMessage(`Something went wrong during the purchase: ${handleError(error)}`, "error-message");
-      }
-    }
-  };
+    return (
+        <div className="background-container">
+            <BaseContainer>
+                <div className="shop container">
+                    <div className="shop form"><br /><br />
 
-  const displayMessage = (messageText, messageType) => {
-    setMessage({ text: messageText, type: messageType });
-    setTimeout(() => {
-      setMessage({ text: "", type: "" });
-    }, 5000); // Hide message after 5 seconds
-  };
+                        {/* Display remaining time */}
+                        <div style={{ position: 'absolute', top: 50, left: 100, textAlign: 'center'}}>
+                            Time: <br />
+                            {timeLeft}
+                        </div>
 
-  return (
-    <div className="background-container">
-      <BaseContainer>
-        <div className="shop container">
-          <div className="shop form"><br /><br />
+                        {/* Display Points */}
+                        <div style={{ position: 'absolute', top: 50, right: 100, textAlign: 'center'}}>
+                            Your Point: <br />
+                            {player.score}
+                        </div>
 
-            {/* Display remaining time */}
-            <div style={{ position: 'absolute', top: 50, left: 100, textAlign: 'center'}}>
-              Time: <br />
-              {timeLeft}
-            </div>
+                        {/* Get Hints */}
+                        <div style={{ textAlign: 'left'}}>
+                            Hints: 20 Points
+                        </div>
+                        <div className="register button-container" style={{display: "flex",justifyContent: 'space-between', margin: 0, padding: 0}} >
+                            <button className="shop hint"></button>
+                            <button className="shop buy-button"
+                                    onClick={() => doPurchase("HINT")}
+                            >
+                                Buy
+                            </button>
+                        </div>
+                        <div style={{ fontSize: "10px", fontFamily: '"Microsoft YaHei", sans-serif' }}>
+                            You can use this tool to get hints.
+                        </div><br />
 
-            {/* Display Points */}
-            <div style={{ position: 'absolute', top: 50, right: 100, textAlign: 'center'}}>
-              Your Point: <br />
-              myScore
-            </div>
+                        {/* Disturb Others */}
+                        <div style={{ textAlign: 'left'}}>
+                            Blur: 20 Points
+                        </div>
+                        <div className="register button-container" style={{display: "flex",justifyContent: 'space-between', margin: 0, padding: 0}} >
+                            <button className="shop bomb"></button>
+                            <button className="shop buy-button"
+                                    onClick={() => doPurchase("BLUR")}
+                            >
+                                Buy
+                            </button>
+                        </div>
+                        <div style={{ fontSize: "10px", fontFamily: '"Microsoft YaHei", sans-serif' }}>
+                            You can use this tool to disturb other player.
+                        </div><br />
 
-            {/* Get Hints */}
-            <div style={{ textAlign: 'left'}}>
-              Hints: 20 Points
-            </div>
-            <div className="register button-container" style={{display: "flex",justifyContent: 'space-between', margin: 0, padding: 0}} >
-              <button className="shop hint"></button>
-              <button className="shop buy-button"
-                onClick={() => doPurchase("HINT")}
-              >
-                Buy
-              </button>
-            </div>
-            <div style={{ fontSize: "10px", fontFamily: '"Microsoft YaHei", sans-serif' }}>
-              You can use this tool to get hints.
-            </div><br />
-
-            {/* Disturb Others */}
-            <div style={{ textAlign: 'left'}}>
-              Blur: 20 Points
-            </div>
-            <div className="register button-container" style={{display: "flex",justifyContent: 'space-between', margin: 0, padding: 0}} >
-              <button className="shop bomb"></button>
-              <button className="shop buy-button"
-                onClick={() => doPurchase("BLUR")}
-              >
-                Buy
-              </button>
-            </div>
-            <div style={{ fontSize: "10px", fontFamily: '"Microsoft YaHei", sans-serif' }}>
-              You can use this tool to disturb other player.
-            </div><br />
-
-            <div className="shop button-container">
+                        {/*<div className="shop button-container">
               <Button
                 width="100%"
                 onClick={() => navigate("/gameRoom")}
               >
                 Skip
               </Button>
-            </div>
-            {/* Display message */}
-            {message.text && (
-                <div style={{ fontSize: "16px", fontFamily: '"Microsoft YaHei", sans-serif' }}>
-                {/*<div className={`message-container ${message.type}`}>*/}
-                  {message.text}
+            </div>*/}
+                        {/* Display message */}
+                        {message.text && (
+                            <div style={{ fontSize: "16px", fontFamily: '"Microsoft YaHei", sans-serif' }}>
+                                {/*<div className={`message-container ${message.type}`}>*/}
+                                {message.text}
+                            </div>
+                        )}
+                    </div>
                 </div>
-            )}
-          </div>
+            </BaseContainer>
         </div>
-      </BaseContainer>
-    </div>
-  );
+    );
 };
 
 /**
